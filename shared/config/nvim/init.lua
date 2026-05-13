cbff0893e26a5261a674911066138792db20e837
Sokpiseth Thin <sokpiseth.thin@gmail.com>
2026-05-13 10:20:39 +0700

    better harper

--- shared/config/nvim/init.lua
+++ shared/config/nvim/init.lua
@@ -1368,14 +1368,37 @@ local function setup_lsp()
         harper_ls = {
             filetypes = { "typst", "tex", "markdown" },
             settings = {
-                ["harper-ls"] = {
+                ["harper-ls"] =
+                {
+                    userDictPath = "",
+                    workspaceDictPath = "",
+                    fileDictPath = "",
                     linters = {
                         SpellCheck = true,
-                        SentenceCapitalization = false,
-                        UnclosedQuotes = true,
+                        SpelledNumbers = false,
+                        AnA = true,
+                        SentenceCapitalization = true,
+                        -- UnclosedQuotes = true,
+                        WrongQuotes = false,
+                        LongSentences = true,
                         RepeatedWords = true,
+                        Spaces = true,
+                        Matcher = true,
+                        CorrectNumberSuffix = true
                     },
-                },
+                    codeActions = {
+                        ForceStable = false
+                    },
+                    markdown = {
+                        IgnoreLinkTitle = false
+                    },
+                    diagnosticSeverity = "hint",
+                    isolateEnglish = false,
+                    dialect = "American",
+                    maxFileLength = 120000,
+                    ignoredLintsPath = "",
+                    excludePatterns = {}
+                }
             },
         },
         tinymist = {},
